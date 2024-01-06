155b1fc47a08d30fe2a2d8646a123111196ed9d7
matejfric <matejfric@centrum.cz>
2024-01-06 12:45:33 +0100

    fix optimizers; clean up

--- mlp/layers.py
+++ mlp/layers.py
@@ -1,7 +1,7 @@
 import numpy as np
+import logging
 from pydantic import PositiveFloat, PositiveInt
 from abc import ABC, abstractmethod
-import logging
 
 from .activations import Activation
 from .constants import SEED
@@ -12,7 +12,7 @@ class Layer(ABC):
     def __init__(self, n_neurons: PositiveInt, *args, **kwargs):
         self.rng = np.random.default_rng(SEED)
         self.n_neurons: PositiveInt = n_neurons  # #nodes_out
-        self.activation: Activation = None
+        self.activation: Activation = None # activation function
         self._weights = None  # Matrix (#nodes_in, #nodes_out)
         self._biases = None  # Matrix (1, #n_neurons)
         self._gradient_w = None  # cost_gradient_w
@@ -20,10 +20,11 @@ class Layer(ABC):
         self._inputs = None
         self._weighted_inputs = None
         self._activations = None
-        self._weight_m = None
-        self._bias_m = None
-        self._weight_v = None
-        self._bias_v = None
+        self._weight_m = None  # 1st stat. moment cache
+        self._bias_m = None  # 1st stat. moment cache
+        self._weight_v = None  # 2nd stat. moment cache
+        self._bias_v = None  # 2nd stat. moment cache
+        self._t = 0  # time for Adam
 
     @property
     def gradient_w(self):
@@ -57,10 +58,6 @@ class Layer(ABC):
     def forward(self, inputs: np.ndarray):
         raise NotImplementedError()
 
-    @abstractmethod
-    def backward(self, inputs: np.ndarray):
-        raise NotImplementedError()
-
 
 class Input(Layer):
     def __init__(self, n_neurons):
@@ -70,9 +67,6 @@ class Input(Layer):
         self._activations = inputs
         return inputs
 
-    def backward(self, inputs):
-        return inputs
-
 
 class Dense(Layer):
     def __init__(self, n_neurons: PositiveInt, activation: Activation):
@@ -80,7 +74,6 @@ class Dense(Layer):
         self.activation = activation
         self._weights = None
         self._biases = None
-        # self.output = None
 
     def set_input(self, n_inputs: PositiveInt):
         self._weights = 0.10 * \
@@ -99,13 +92,10 @@ class Dense(Layer):
         self._inputs = inputs
         self._weighted_inputs = np.dot(inputs, self._weights) + self._biases
         self._activations = self.activation.apply(self._weighted_inputs)
-        # self.output = np.dot(inputs, self.weights) + self.biases
-        # self.output = self.activation.activate(self.output)
-        return self._activations  # self.output
+        return self._activations
 
     def apply_gradients(self,
                         optimizer: Optimizer,
-                        regularization: PositiveFloat,
                         learning_rate: PositiveFloat,
                         momentum: PositiveFloat):
         optimizer_functions = {
@@ -115,13 +105,11 @@ class Dense(Layer):
             Optimizer.RMSPROP: self._rmsprop,
             Optimizer.ADAM: self._adam,
         }
-        # Call the function corresponding to the optimizer
-        optimize = optimizer_functions.get(optimizer,
-                                lambda: logging.warning("Invalid optimizer"))
-        optimize(regularization, learning_rate, momentum)
+        optimize = optimizer_functions\
+            .get(optimizer, lambda: logging.warning("Invalid optimizer"))
+        optimize(learning_rate, momentum)
 
-    def _sgd(self, 
-             regularization: PositiveFloat,
+    def _sgd(self,
              learning_rate: PositiveFloat,
              momentum: PositiveFloat):
         """Stochastic gradient descent (SGD)"""
@@ -129,11 +117,10 @@ class Dense(Layer):
         self._biases -= self.gradient_b * learning_rate
 
     def _sgd_momentum(self,
-                      regularization: PositiveFloat,
                       learning_rate: PositiveFloat,
                       momentum: PositiveFloat):
         """
-        Stochastic gradient descent (with momentum),
+        Stochastic gradient descent with momentum,
         if momentum=0, then this becomes regular SGD.
         """
         gw = self.gradient_w  # gradient for weights 
@@ -147,21 +134,19 @@ class Dense(Layer):
         self._biases += self._bias_m
 
     def _adagrad(self,
-                 regularization: PositiveFloat,
                  learning_rate: PositiveFloat,
                  momentum: PositiveFloat,
                  epsilon: PositiveFloat = 1e-8):
         """ ADAGrad """
         gw = self.gradient_w  # gradient for weights 
         self._weight_v += np.square(gw)  # cache of squared gradients
-        self._weights -= learning_rate * gw / np.sqrt(self._weight_v + epsilon)
+        self._weights -= learning_rate * gw / (np.sqrt(self._weight_v) + epsilon)
 
         gb = self.gradient_b  # gradient for biases 
         self._bias_v += np.square(gb)  # cache of squared gradients
-        self._biases -= learning_rate * gb / np.sqrt(self._bias_v + epsilon)
+        self._biases -= learning_rate * gb / (np.sqrt(self._bias_v) + epsilon)
 
     def _rmsprop(self,
-                 regularization: PositiveFloat,
                  learning_rate: PositiveFloat,
                  momentum: PositiveFloat,
                  decay: PositiveFloat = 0.999,
@@ -170,34 +155,42 @@ class Dense(Layer):
         gw = self.gradient_w  # gradient for weights 
         vw = self._weight_v  # cache of squared gradients
         self._weight_v = decay * vw + (1-decay) * np.square(gw)
-        self._weights -= learning_rate * gw / np.sqrt(self._weight_v + epsilon)
+        self._weights -= learning_rate * gw / (np.sqrt(self._weight_v) + epsilon)
 
         gb = self.gradient_b  # gradient for biases 
         vb = self._bias_v  # cache of squared gradients
         self._bias_v = decay * vb + (1-decay) * np.square(gb)
-        self._biases -= learning_rate * gb / np.sqrt(self._bias_v + epsilon)
+        self._biases -= learning_rate * gb / (np.sqrt(self._bias_v) + epsilon)
 
     def _adam(self,
-                 regularization: PositiveFloat,
                  learning_rate: PositiveFloat = 0.001,
                  momentum: PositiveFloat = 0.9,
                  decay: PositiveFloat = 0.999,
                  epsilon: PositiveFloat = 1e-8):
-        """ Adaptive Momentum Estimator (Adam) """
+        """ 
+        Adaptive Momentum Estimator (Adam) 
+        
+        Inspired by:
+          - https://medium.com/the-ml-practitioner/how-to-implement-an-adam-optimizer-from-scratch-76e7b217f1cc
+        """
+
+        self._t += 1
+        t = self._t
+
         gw = self.gradient_w  # gradient for weights 
         mw = self._weight_m  # cache of gradients
         vw = self._weight_v  # cache of squared gradients
 
         # Estimate 1st stat. moment
         self._weight_m = momentum * mw + (1-momentum) * gw
-        self._weight_m /= (1-momentum)
+        mw_corr = self._weight_m / (1-(momentum**(t)))  # correction
 
         # Estimate 2nd stat. moment
         self._weight_v = decay * vw + (1-decay) * np.square(gw)
-        self._weight_v /= (1-decay)
+        vw_corr = self._weight_v / (1-(decay**(t)))  # correction
 
         # Update weigths
-        self._weights -= learning_rate * self._weight_m / np.sqrt(self._weight_v + epsilon)
+        self._weights -= learning_rate * mw_corr / (np.sqrt(vw_corr) + epsilon)
 
         # Likewise for biases
         gb = self.gradient_b  # gradient for biases 
@@ -206,15 +199,11 @@ class Dense(Layer):
 
         # Estimate 1st stat. moment
         self._bias_m = momentum * mb + (1-momentum) * gb
-        self._bias_m /= (1-momentum)
+        mb_corr = self._bias_m / (1-(momentum**(t)))
 
         # Estimate 2nd stat. moment
         self._bias_v = decay * vb + (1-decay) * np.square(gb)
-        self._bias_v /= (1-decay)
+        vb_corr = self._bias_v / (1-(decay**(t)))
 
         # Update biases
-        self._biases -= learning_rate * self._bias_m / np.sqrt(self._bias_v + epsilon)
-
-    # TODO:
-    def backward(self, inputs: np.ndarray):
-        return super().backward(inputs)
+        self._biases -= learning_rate * mb_corr / (np.sqrt(vb_corr) + epsilon)
